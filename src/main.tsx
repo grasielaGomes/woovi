70a5f288dcf0a9ab6d45b893b34205777c404633
Grasiela Gomes <produtoradama@gmail.com>
2022-10-21 15:45:10 -0300

    feat: add theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
+import { ThemeProvider } from '@emotion/react';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { lightTheme } from './themes/light-theme';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
-)
+);
