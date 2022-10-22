f1647bd1dff55575fa069a6637c316ef6b171987
Grasiela Gomes <produtoradama@gmail.com>
2022-10-22 18:58:28 -0300

    feat: add Identifier

---
+++ src/components/pix-credit-card/identifier.tsx
@@ -0,0 +1,14 @@
+import { Stack, Typography } from "@mui/material";
+
+export const Identifier = () => {
+  return (
+    <Stack alignItems="center">
+      <Typography variant="caption" color="neutral.main">
+        Identificador:
+      </Typography>
+      <Typography variant="caption" fontWeight="bold">
+        2c1b951f356c4680b13ba1c9fc889c47
+      </Typography>
+    </Stack>
+  );
+};
