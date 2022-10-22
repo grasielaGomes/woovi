5ec20931eb792f88ae2b0fab7497cf657d29ae17
Grasiela Gomes <produtoradama@gmail.com>
2022-10-22 13:33:02 -0300

    refactor: import type

--- src/components/templates/base-template.tsx
+++ src/components/templates/base-template.tsx
@@ -1,11 +1,10 @@
 import { FC, HTMLAttributes } from "react";
 import { Container, Stack } from "@mui/material";
+import { DivAttributes } from "../../interfaces";
 
-type BaseTemplateProps = HTMLAttributes<HTMLDivElement>;
-
-export const BaseTemplate: FC<BaseTemplateProps> = (props) => {
+export const BaseTemplate: FC<DivAttributes> = (props) => {
   return (
-    <Container maxWidth="xs" sx={{ py: 3, px: 1 }} {...props}>
+    <Container maxWidth="sm" sx={{ py: 3, px: 1 }} {...props}>
       <Stack spacing={4}>{props.children}</Stack>
     </Container>
   );
