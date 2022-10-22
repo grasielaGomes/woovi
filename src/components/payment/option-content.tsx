6cfcc5f6a4787a2ea9e97b13e491c5b4384f7fb6
Grasiela Gomes <produtoradama@gmail.com>
2022-10-21 22:50:27 -0300

    feat: add OptionContent

---
+++ src/components/payment/option-content.tsx
@@ -0,0 +1,36 @@
+import { FC } from "react";
+import { Stack, Typography } from "@mui/material";
+import { PixI } from "../../interfaces";
+import { fontWeight } from "../../themes";
+
+export const OptionContent: FC<PixI> = ({
+  amount,
+  cashback,
+  installment,
+  total
+}) => {
+  return (
+    <Stack>
+      <Typography variant="heading1" display="inline" textAlign="left">
+        {`${installment}x `}
+        <span style={{ fontWeight: fontWeight.regular }}>{amount}</span>
+      </Typography>
+      {total && (
+        <Typography
+          textAlign="left"
+          variant="body2"
+          color="neutral.200"
+        >{`Total: ${total}`}</Typography>
+      )}
+      {cashback && (
+        <Typography variant="body2" color="primary" display="inline">
+          Ganhe
+          <span
+            style={{ fontWeight: fontWeight.bold }}
+          >{` ${cashback}% `}</span>
+          de Cashback
+        </Typography>
+      )}
+    </Stack>
+  );
+};
