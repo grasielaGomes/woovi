a2d5df164d9457a4b7d08e271b203fa7aa273e20
Grasiela Gomes <produtoradama@gmail.com>
2022-10-21 22:50:35 -0300

    feat: add Options

---
+++ src/components/payment/options.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react";
+import { ButtonGroup, Chip } from "@mui/material";
+import { PixI } from "../../interfaces";
+import { OptionCard } from "../../components";
+
+interface OptionsI {
+  methodLabel: string;
+  options: PixI[];
+  selectedOption: number;
+}
+
+export const Options: FC<OptionsI> = ({
+  options,
+  selectedOption,
+  methodLabel
+}) => {
+  return (
+    <ButtonGroup orientation="vertical" sx={{ position: "relative" }}>
+      <Chip
+        label={methodLabel}
+        sx={{ position: "absolute", top: -16, left: 20, zIndex: 10 }}
+      />
+      {options.map((option) => {
+        return (
+          <OptionCard
+            key={option.installment}
+            selectedOption={selectedOption}
+            {...option}
+          />
+        );
+      })}
+    </ButtonGroup>
+  );
+};
