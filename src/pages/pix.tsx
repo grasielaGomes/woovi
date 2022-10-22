bc1e0277cf3980421205f3e88d33d465860a0210
Grasiela Gomes <produtoradama@gmail.com>
2022-10-22 18:57:11 -0300

    refactor: add new components

--- src/pages/pix.tsx
+++ src/pages/pix.tsx
@@ -1,3 +1,31 @@
+import { Divider, Stack } from "@mui/material";
+import { pixCode } from "../assets";
+import {
+  BaseTemplate,
+  PageHeader,
+  PixQRCode,
+  DueDate,
+  Installments,
+  Total,
+  HowItWorks,
+  Identifier,
+  PageFooter
+} from "../components";
+
 export const Pix = () => {
-  return <h1>Hello, Pxi</h1>
-}
+  return (
+    <BaseTemplate>
+      <PageHeader title="João, pague a entrada de R$ 15.300,00 pelo Pix" />
+      <PixQRCode code={pixCode} />
+      <DueDate date="15/12/2021 - 08:17" />
+      <Stack spacing={2}>
+        <Installments />
+        <Divider />
+        <Total />
+        <HowItWorks />
+      </Stack>
+      <Identifier />
+      <PageFooter />
+    </BaseTemplate>
+  );
+};
