12d36b6bb9bf7bed39f3770d5263fc01b0dcc5b3
Grasiela Gomes <produtoradama@gmail.com>
2022-10-22 20:46:55 -0300

    refactor: add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 function App() {
-
   return (
-    <h1>Hello, World!</h1>
-  )
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
