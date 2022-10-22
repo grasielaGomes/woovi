63d4f62cd9a2ad53d337c6e074ade75f3c43617e
Grasiela Gomes <produtoradama@gmail.com>
2022-10-22 20:47:32 -0300

    lint: change quotes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import { ThemeProvider } from '@emotion/react';
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { lightTheme } from './themes/light-theme';
+import { ThemeProvider } from "@mui/material";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { lightTheme } from "./themes/light-theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
