7ccc54c63e0609d8b2b35f35bb455fb5c7261259
Grasiela Gomes <produtoradama@gmail.com>
2022-10-22 13:34:21 -0300

    feat: add RadioIcon

---
+++ src/components/icons/radio-icon.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import { SvgIcon } from "@mui/material";
+import { IconAttributes } from "../../interfaces";
+
+export const RadioIcon: FC<IconAttributes> = (props) => {
+  return (
+    <SvgIcon {...props}>
+      <svg viewBox="0 0 26 26" fill="none">
+        <circle cx="13" cy="13" r="12" stroke="#E5E5E5" stroke-width="2" />
+      </svg>
+    </SvgIcon>
+  );
+};
