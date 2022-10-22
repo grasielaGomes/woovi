3fc88bb3b7049930d580c82841b86a4d4ec19d16
Grasiela Gomes <produtoradama@gmail.com>
2022-10-22 18:57:56 -0300

    refactor: clean imports

--- src/components/templates/base-template.tsx
+++ src/components/templates/base-template.tsx
@@ -1,11 +1,13 @@
-import { FC, HTMLAttributes } from "react";
+import { FC } from "react";
 import { Container, Stack } from "@mui/material";
 import { DivAttributes } from "../../interfaces";
 
 export const BaseTemplate: FC<DivAttributes> = (props) => {
   return (
     <Container maxWidth="sm" sx={{ py: 3, px: 1 }} {...props}>
-      <Stack spacing={4}>{props.children}</Stack>
+      <Stack spacing={4}>
+        {props.children}
+      </Stack>
     </Container>
   );
 };
