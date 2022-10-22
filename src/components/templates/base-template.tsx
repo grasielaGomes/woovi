853e6680c7b3a7784f9943b3d72846c291aaf314
Grasiela Gomes <produtoradama@gmail.com>
2022-10-21 22:50:53 -0300

    refactor: add Stack component

--- src/components/templates/base-template.tsx
+++ src/components/templates/base-template.tsx
@@ -1,12 +1,12 @@
 import { FC, HTMLAttributes } from "react";
-import { Container } from "@mui/material";
+import { Container, Stack } from "@mui/material";
 
 type BaseTemplateProps = HTMLAttributes<HTMLDivElement>;
 
 export const BaseTemplate: FC<BaseTemplateProps> = (props) => {
   return (
     <Container maxWidth="xs" sx={{ py: 3, px: 1 }} {...props}>
-      {props.children}
+      <Stack spacing={4}>{props.children}</Stack>
     </Container>
   );
 };
