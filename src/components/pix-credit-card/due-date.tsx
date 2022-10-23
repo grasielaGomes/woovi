a9b6ba6d71ce2bf78484f6ca3fed38efba184f43
Grasiela Gomes <produtoradama@gmail.com>
2022-10-23 10:48:26 -0300

    refactor: change external prop to store

--- src/components/pix-credit-card/due-date.tsx
+++ src/components/pix-credit-card/due-date.tsx
@@ -1,17 +1,15 @@
 import { FC } from "react";
 import { Stack, Typography } from "@mui/material";
+import { pixCreditCardStore } from "../../stores";
 
-interface DueDateI {
-  date: string
-}
-
-export const DueDate: FC<DueDateI> = ({date}) => {
+export const DueDate: FC = () => {
+  const { dueDate } = pixCreditCardStore.getState();
   return (
     <Stack alignItems="center">
       <Typography variant="body2" color="neutral.main">
         Prazo de pagamento:
       </Typography>
-      <Typography variant="body1">{date}</Typography>
+      <Typography variant="body1">{dueDate}</Typography>
     </Stack>
   );
 };
