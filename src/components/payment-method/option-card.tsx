5eaf87d8e37cf18e54f1e0b7178a3756cd909622
Grasiela Gomes <produtoradama@gmail.com>
2022-10-23 10:47:55 -0300

    feat: add onClick and store

--- src/components/payment-method/option-card.tsx
+++ src/components/payment-method/option-card.tsx
@@ -4,30 +4,42 @@ import { Stack, Button } from "@mui/material";
 import { OptionContent, CheckOption, Flag } from "../../components";
 import { OptionContentI } from "../../interfaces";
 import { SELECTED_OPTION as styles } from "../../styles";
+import { paymentOptionsStore } from "../../stores";
 
 export const OptionCard: FC<OptionContentI> = ({
   amount,
   cashback,
+  cet,
   flag,
   installment,
   selectedOption,
   total
 }) => {
+  
   const isSelected = selectedOption === installment;
+
   let navigate = useNavigate();
+  
+  const onClick = () => {
+    const state = paymentOptionsStore.getState();
+    paymentOptionsStore.setState({
+      ...state,
+      selectedOption: state.options.find(
+        (option) => option.installment === installment
+      )!
+    });
+    navigate("/pix")
+  };
+  
   return (
-    <Button
-      variant="outlined"
-      sx={isSelected ? styles : {}}
-      onClick={() => navigate("/pix")}
-    >
+    <Button variant="outlined" sx={isSelected ? styles : {}} onClick={onClick}>
       <Stack width="100%" py={2} px={1} spacing={1}>
         <Stack
           direction="row"
           justifyContent="space-between"
           alignItems="start"
         >
-          <OptionContent {...{ amount, cashback, installment, total }} />
+          <OptionContent {...{ amount, cashback, cet, installment, total }} />
           <CheckOption isSelected={isSelected} />
         </Stack>
         {flag && <Flag>{flag}</Flag>}
