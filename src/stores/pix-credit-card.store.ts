745827cee1dc378871a64177f50ca79dddad0e95
Grasiela Gomes <produtoradama@gmail.com>
2022-10-23 14:56:29 -0300

    refactor: change asset path

--- src/stores/pix-credit-card.store.ts
+++ src/stores/pix-credit-card.store.ts
@@ -4,7 +4,7 @@ import { formatDate } from '../utils';
 const formattedDate = formatDate(new Date())
 
 export const pixCreditCardStore = createStore({
-  code: "src/assets/qr-code.svg",
+  code: "qr-code.svg",
   dueDate: formattedDate,
   identifier: "2c1b951f356c4680b13ba1c9fc889c47"
 });
