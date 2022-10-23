9fdfa8cf1ee6e954da1117b9af3c4f6626912c3d
Grasiela Gomes <produtoradama@gmail.com>
2022-10-23 10:50:46 -0300

    refactor: add cet values

--- src/helpers/payment-methods.helper.ts
+++ src/helpers/payment-methods.helper.ts
@@ -4,37 +4,45 @@ export const PIX_OPTIONS: PixOptionsType = [
   {
     amount: 30500.0,
     cashback: 3,
+    cet: 0,
     flag: "🤑 R$ 300,00 de volta no seu Pix na hora",
-    installment: 1
+    installment: 1,
+    total: 30500.0
   },
   {
     amount: 15300.0,
+    cet: 0.5,
     installment: 2,
     total: 30600.0
   },
   {
     amount: 10196.66,
+    cet: 0.5,
     installment: 3,
     total: 30620.0
   },
   {
     amount: 7725.0,
+    cet: 0.5,
     flag: "-3% de juros: Melhor opção de parcelamento",
     installment: 4,
     total: 30900.0
   },
   {
     amount: 6300.0,
+    cet: 0.5,
     installment: 5,
     total: 31500.0
   },
   {
     amount: 5283.33,
+    cet: 0.5,
     installment: 6,
     total: 31699.98
   },
   {
     amount: 4542.85,
+    cet: 0.5,
     installment: 7,
     total: 31800.0
   }
