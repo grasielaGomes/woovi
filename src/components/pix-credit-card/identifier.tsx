4de4dd91ad1cb8ddf874d0bda7e32e269c922290
Grasiela Gomes <produtoradama@gmail.com>
2022-10-23 10:49:04 -0300

    refactor: add identifier from store

--- src/components/pix-credit-card/identifier.tsx
+++ src/components/pix-credit-card/identifier.tsx
@@ -1,13 +1,15 @@
 import { Stack, Typography } from "@mui/material";
+import { pixCreditCardStore } from "../../stores";
 
 export const Identifier = () => {
+  const { identifier } = pixCreditCardStore.getState();
   return (
     <Stack alignItems="center">
       <Typography variant="caption" color="neutral.main">
         Identificador:
       </Typography>
       <Typography variant="caption" fontWeight="bold">
-        2c1b951f356c4680b13ba1c9fc889c47
+        {identifier}
       </Typography>
     </Stack>
   );
