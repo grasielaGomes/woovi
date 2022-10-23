3a62fc7693a94a5cc131adc68ae1a5c99e805ec5
Grasiela Gomes <produtoradama@gmail.com>
2022-10-23 14:13:42 -0300

    feat: export new file

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,4 +1,5 @@
 export * from './create-store'
+export * from './credit-card'
 export * from './installment-step.store'
 export * from './payment-options.store'
 export * from './pix-credit-card.store'
