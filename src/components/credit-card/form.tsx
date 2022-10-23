110c93ebe9755091c1a7eff7aeca0efc08343025
Grasiela Gomes <produtoradama@gmail.com>
2022-10-23 14:13:11 -0300

    feat: add useCreditCard hook

--- src/components/credit-card/form.tsx
+++ src/components/credit-card/form.tsx
@@ -1,13 +1,25 @@
-import { useNavigate } from "react-router-dom";
 import { Button, MenuItem, Stack, TextField } from "@mui/material";
+import { useCreditCard } from "../../hooks";
+import { crediCardStore } from "../../stores";
 
 export const CreditCardForm = () => {
-  let navigate = useNavigate();
+  const { fullName, cvv } = crediCardStore.useStore((state) => state.fields);
+  const {
+    maskedCardNumber,
+    maskedCpf,
+    maskedExpiration,
+    handleCardNumberChange,
+    handleCpfChange,
+    handleExpirationChange,
+    handleInputChange,
+    isValidated,
+    onSubmit
+  } = useCreditCard();
   return (
     <Stack
       spacing={3}
       component="form"
-      noValidate
+      noValidate={!isValidated}
       autoComplete="off"
       sx={{
         "& .MuiInputBase-root": {
@@ -16,42 +28,72 @@ export const CreditCardForm = () => {
       }}
     >
       <TextField
+        error={isValidated && !fullName}
+        fullWidth
+        helperText={isValidated && !fullName && "Nome completo é obrigatório"}
         id="full-name"
-        required
         label="Nome completo"
-        variant="outlined"
-        fullWidth
-      />
-      <TextField id="cpf" required label="CPF" variant="outlined" fullWidth />
-      <TextField
-        id="card-number"
+        onChange={(event) => handleInputChange(event, "fullName")}
         required
-        label="Número do cartão"
         variant="outlined"
+      />
+      <TextField
+        error={isValidated && !maskedCpf}
         fullWidth
+        helperText={isValidated && !maskedCpf && "CPF é obrigatório"}
+        id="cpf"
+        inputProps={{ inputMode: "numeric", pattern: "[0-9]*", maxLength: 14 }}
+        label="CPF"
+        onChange={handleCpfChange}
+        required
+        type="tel"
+        value={maskedCpf}
+        variant="outlined"
+      />
+      <TextField
+        error={isValidated && !maskedCardNumber}
+        fullWidth
+        helperText={isValidated && !maskedCpf && "Número do é obrigatório"}
+        id="card-number"
+        inputProps={{ inputMode: "numeric", pattern: "[0-9]*", maxLength: 19 }}
+        label="Número do cartão"
+        onChange={handleCardNumberChange}
+        required
+        type="tel"
+        value={maskedCardNumber}
+        variant="outlined"
       />
       <Stack direction="row" spacing={3}>
         <TextField
-          id="expiration"
-          required
-          label="Vencimento"
-          type="tel"
-          variant="outlined"
+          error={isValidated && !maskedExpiration}
           fullWidth
+          helperText={isValidated && !maskedCpf && "Validade é obrigatório"}
+          id="expiration"
+          inputProps={{ inputMode: "numeric", pattern: "[0-9]*", maxLength: 5 }}
+          label="Vencimento"
+          onChange={handleExpirationChange}
+          required
+          type="tel"
+          value={maskedExpiration}
+          variant="outlined"
         />
         <TextField
+          error={isValidated && !cvv}
+          fullWidth
+          helperText={isValidated && !maskedCpf && "CVV é obrigatório"}
           id="cvv"
-          required
+          inputProps={{ inputMode: "numeric", pattern: "[0-9]*", maxLength: 3 }}
           label="CVV"
+          onChange={(event) => handleInputChange(event, "cvv")}
+          required
           type="tel"
           variant="outlined"
-          fullWidth
         />
       </Stack>
       <TextField
         id="installments"
-        select
         label="Parcelas"
+        select
         value="1x de R$ 15.300,00"
       >
         {["1x de R$ 15.300,00"].map((option) => (
@@ -60,7 +102,7 @@ export const CreditCardForm = () => {
           </MenuItem>
         ))}
       </TextField>
-      <Button variant="contained" fullWidth onClick={() => navigate("/")}>
+      <Button variant="contained" fullWidth onClick={onSubmit}>
         Pagar
       </Button>
     </Stack>
